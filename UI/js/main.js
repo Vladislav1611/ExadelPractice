d65dfff5a00d2fde22861da8552576202c4357ba
Vladislav Parchevscky <parchevscky16@gmail.com>
2018-03-11 14:31:10 +0300

    add hashtags,like

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,223 +1,186 @@
 'strict mode';
 (function () {
-    var photoPosts = [
-        {
-            id: '1',
-            descriprion: 'test 1!!!',
-            createdAt: new Date('2018-02-10T23:00:00'),
-            author: 'Иванов Иван',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-        },
-        {
-            id: '2',
-            descriprion: 'test 2!!!',
-            createdAt: new Date('2018-02-24T23:00:00'),
-            author: '4 test',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-        },
-        {
-            id: '3',
-            descriprion: 'test 3!!!',
-            createdAt: new Date('2018-02-12T23:00:00'),
-            author: 'Иванов Иван',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-        },
-        {
-            id: '4',
-            descriprion: 'test 4!!!',
-            createdAt: new Date('2018-07-19T23:00:00'),
-            author: 'Иванов Иван',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-        },
-        {
-            id: '5',
-            descriprion: 'test 4!!!',
-            createdAt: new Date('2018-07-19T23:00:00'),
-            author: '4 test',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-        },
-        {
-            id: '6',
-            descriprion: 'test 4!!!',
-            createdAt: new Date('2018-07-19T23:00:00'),
-            author: 'Иванов Иван',
-            photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-        }
-    ];
-
-//console.log(getPhotoPosts(3));
-//console.log(getPhotoPost('5'));
-//validatePhotoPost(4,'asdasdasd',new Date('2018-07-19T23:00:00'),'Иванов Иван','http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg');
-//addPhotoPosts('7', 'lox', new Date('2018-02-10T23:00:00'), 'vlad', 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg');
-//console.log('remove' + removePhotoPost('5'));
-//editPhotoPost('3','3', 'lox', new Date('2018-02-10T23:00:00'), 'vlad', 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg')
-
-
-
-
-
-    function getPhotoPosts(skip, top, filterConfig) {
-        let tmp = [];
-        if (filterConfig instanceof  Object && Object.keys(filterConfig).length < 2) {
-            photoPosts.forEach( function (item) {
-                if (item.author === filterConfig.author) {
-                    tmp.push(item);
-                }
-            });
-            if (tmp.length === 0) {
-                tmp = photoPosts;
+        var photoPosts = [
+            {
+                id: '1',
+                descriprion: 'test 1!!!',
+                createdAt: new Date('2018-02-10T23:00:00'),
+                author: 'Иванов Иван',
+                photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+                hashTag: ["#Minsk", "#Famcs"],
+                like: ["Vlad", "Andrew"],
+            },
+            {
+                id: '2',
+                descriprion: 'test 2!!!',
+                createdAt: new Date('2018-02-24T23:00:00'),
+                author: '4 test',
+                photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+                hashTag: ["#Minsk", "#Famcs"],
+                like: ["Vlad", "Andrew"],
+            },
+            {
+                id: '3',
+                descriprion: 'test 3!!!',
+                createdAt: new Date('2018-02-12T23:00:00'),
+                author: 'Иванов Иван',
+                photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+                hashTag: ["#Minsk", "#Famcs"],
+                like: ["Vlad", "Andrew"],
+            },
+            {
+                id: '4',
+                descriprion: 'test 4!!!',
+                createdAt: new Date('2018-07-19T23:00:00'),
+                author: 'Иванов Иван',
+                photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+                hashTag: ["#Minsk", "#Famcs"],
+                like: ["Vlad", "Ivan"],
+            },
+            {
+                id: '5',
+                descriprion: 'test 4!!!',
+                createdAt: new Date('2018-07-19T23:00:00'),
+                author: '4 test',
+                photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+                hashTag: ["#Minsk", "#BNTY"],
+                like: ["Vlad", "Andrew"],
+            },
+            {
+                id: '6',
+                descriprion: 'test 4!!!',
+                createdAt: new Date('2018-07-19T23:00:00'),
+                author: 'Иванов Иван',
+                photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+                hashTag: ["#Minsk", "#Moscow"],
+                like: ["Vlad", "Andrew"],
             }
-            filterData(tmp);
-        } else {
-            tmp = photoPosts;
-            filterData(tmp);
-            alert ('You enter not valid data in filter config. Filter config have next structure: {author: "name_author"} ');
-        }
-        return skipTopFilter(skip, top, tmp);
-    }
-    function filterData(arr) {
-        let dataFilterArray = arr;
-        for (let i = 0; i < dataFilterArray.length-1; i++) {
-            for (let j=0; j < dataFilterArray.length-1-i; j++){
-                let tmp1 = dataFilterArray[j].createdAt.getTime();
-                let tmp2 = dataFilterArray[j+1].createdAt.getTime();
+        ];
 
-                if (tmp2 < tmp1) {
-                    let v = dataFilterArray[j+1];
-                    dataFilterArray[j+1] = dataFilterArray[j];
-                    dataFilterArray[j] = v;
+        function compareDate(a, b) {
+            return a.createdAt - b.createdAt;
+        }
+
+        function getPhotoPosts(skip, top, filterConfig) {
+            skip = skip || 0;
+            top = top || 0;
+            var newArr = [];
+            if (!filterConfig) {
+                return photoPosts.slice(skip, skip + top).length
+            }
+            else {
+                var result = photoPosts;
+                if (filterConfig.author) {
+                    result = result.filter(function (post) {
+                        return post.author === filterConfig.author;
+                    })
                 }
+                if (filterConfig.createdAt) {
+                    result = result.filter(function (post) {
+                        return post.createdAt === filterConfig.createdAt;
+                    })
+                }
+                if (filterConfig.hashTag) {
+                    result = result.filter(function (post) {
+                        for (let hashTag of post.hashTag) {
+                            for (let jtem of [].concat(filterConfig.hashTag)) {
+                                if (hashTag === jtem) {
+                                    return true;
+                                }
+                            }
+                        }
+                    });
+                }
+                return result.sort(compareDate).slice(skip, top);
             }
         }
-        return dataFilterArray;
-    }
-    function skipTopFilter(skip, top, array) {
-        let finishArray = [];
-        if ((skip === undefined && top === undefined)) {
-            skip = 0;
-            top = 10;
+
+        function getPhotoPost(id) {
+            var found = photoPosts.find(function (element) {
+                return element.id === id;
+            })
+            return found;
         }
-        else if((skip === undefined && top !== undefined))
-        {
-            skip = 0;
-        }
-        else if((skip !== undefined && top === undefined))
-        {
-            top = 10;
-        }
-        if (skip < array.length) {
-            if ( skip + top >= array.length) {
-                for (let i = skip; i < array.length; i++) {
-                    finishArray.push(array[i]);
-                }
-            } else {
-                for (let i = skip; i < skip+top; i++) {
-                    finishArray.push(array[i]);
+
+        function validatePhotoPost(post) {
+            var isValidate = true;
+            if (typeof post.id !== "string" || typeof post.description !== "string"
+                || typeof post.author !== "string" || typeof post.photoLink !== "string"
+                || !(post.createdAt instanceof Date) || !(post.hashTag instanceof Array)) {
+                return false
+            }
+            for (var i = 0; i < photoPosts.length; i++) {
+                if (photoPosts[i].id === post.id) {
+                    return false;
                 }
             }
-        } else {
-            finishArray = array;
-            alert ('Skip not valid!!! You have array without filter with skip and top parameters');
-        }
-
-        return finishArray;
-    }
-
-    function getPhotoPost(id) {
-
-        let tmp = [];
-        photoPosts.forEach(function (post) {
-            if (post.id === id) {
-                tmp.push(post);
-            }
-        });
-        if(tmp.length >0)
-        {
-            return tmp;
-        }
-        else {
-            console.log('PhotoPost with this id not exist')
-        }
-    }
-
-    function  validatePhotoPost(object) {
-
-        if(typeof object.id !== "string" || typeof object.description !== "string"
-            || typeof object.author !== "string" || typeof object.photoLink !== "string" || !(object.createdAt instanceof Date)){
-            return false
-        }
-        for(let i = 0; i < photoPosts.length; i++){
-            if(photoPosts[i].id === object.id){
+            if (post.description.length === 0 || post.description.length >= 200) {
                 return false;
             }
-        }
-        if(object.description.length === 0 || object.description.length >= 200){
-            return false;
-        }
-        if(object.createdAt === undefined || object.createdAt.toString() === "Invalid Date"){
-            return false;
-        }
-        if(object.author === 0){
-            return false;
-        }
-        return true;
-    }
-
-
-    function addPhotoPosts(object ) {
-        if(validatePhotoPost(object )) {
-            let item = {object};
-            photoPosts.push(item);
-            console.log(photoPosts);
+            if (!post.createdAt || post.createdAt.toString() === "Invalid Date") {
+                return false;
+            }
+            if (post.author.length === 0 || !post.author) {
+                return false;
+            }
             return true;
         }
-        return false;
-    }
 
-    function removePhotoPost(id) {
-        let tmp = [];
-        for(let i=0;i<photoPosts.length;i++) {
-            if(photoPosts[i].id !==id)
-            {
-                tmp.push(photoPosts[i]);
+        function addPhotoPost(post) {
+            if (validatePhotoPost(post)) {
+                console.log(post)
+                if (!post.isDelete) {
+                    post.isDelete = false;
+                }
+                photoPosts[photoPosts.length] = post;
+                return true
+            }
+            return false
+        }
+
+        function removePhotoPost(id) {
+            let tmp = [];
+            for (let i = 0; i < photoPosts.length; i++) {
+                if (photoPosts[i].id !== id) {
+                    tmp.push(photoPosts[i]);
+                }
+            }
+            return tmp;
+        }
+
+        function editPhotoPost(id, object) {
+            for (let i = 0; i < photoPosts.length; i++) {
+                if (photoPosts[i].id === id) {
+                    var temp = photoPosts[i]
+                    if (object.photoLink !== undefined) {
+                        if (object.photoLink.length !== 0) {
+                            temp.photoLink = object.photoLink
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                    if (object.description !== undefined) {
+                        if (object.description.length === 0 || object.description.length >= 200) {
+                            return false;
+                        }
+                        else {
+                            temp.description = object.description
+                        }
+                    }
+                    if (object.hashTag !== undefined) {
+                        if (object.hashTag.length > 0) {
+                            temp.hashTag = object.hashTag
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                    photoPosts[i] = temp
+                    return true;
+                }
             }
         }
-        return tmp;
-    }
-
-    function editPhotoPost(id,object) {
-        for (let i = 0; i < photoPosts.length; i++) {
-            if (photoPosts[i].id === id) {
-                var temp = photoPosts[i]
-                if (object.photoLink !== undefined) {
-                    if (object.photoLink.length !== 0) {
-                        temp.photoLink = object.photoLink
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                if (object.description !== undefined) {
-                    if (object.description.length === 0 || object.description.length >= 200) {
-                        return false;
-                    }
-                    else {
-                        temp.description = object.description
-                    }
-                }
-                if (object.hashTag !== undefined) {
-                    if (object.hashTag.length > 0) {
-                        temp.hashTag = object.hashTag
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                photoPosts[i] = temp
-                return true;
-            }
-        }
-    }
 })();
 
 
